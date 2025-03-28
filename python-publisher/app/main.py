faefa5a08d3665076e59e03c2c49d5daaec0176d
Evan Roggenkamp <evan.roggenkamp@gmail.com>
2025-03-28 17:19:57 -0500

    Updated Python Publisher & Rust Subscriber
    Added retries to publisher. Redid the rust subscriber, and added retries to that as well.

--- python-publisher/app/main.py
+++ python-publisher/app/main.py
@@ -1,12 +1,14 @@
 from datetime import timedelta
+import json
 import logging
-import logging.handlers
 import os
 import random
-import json
+import sys
 import time
+from typing import Literal
 
 import pika
+from pika.adapters.blocking_connection import BlockingChannel
 
 from timeloop import Timeloop
 
@@ -25,6 +27,11 @@ for _logger in ("pika.connection", "pika.adapters", "pika.channel"):
     logging.getLogger(_logger).setLevel(logging.WARNING)
 
 logger = logging.getLogger("python-publisher")
+
+
+class NoExchangeException(Exception):
+    """Custom No Exchange Exception."""
+    pass
 
 
 class Jobs():
@@ -93,36 +100,70 @@ class Jobs():
             logger.warning("CANNOT_SELECT_RANDOM_OPERATION %s", err, exc_info=True)
 
 
-@tl.job(interval=timedelta(seconds=10))  # type: ignore
-def main():
-    connection = None
-    channel = None
+def declare_exchange(channel: BlockingChannel, exchange_name: str, exchange_type: Literal["topic"]) -> bool:
+    """Declare a new Exchange."""
     try:
-        url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@rabbitmq/%2f')
-        params = pika.URLParameters(url)
-        params.socket_timeout = 5
-        params.connection_attempts = 3
-        params.retry_delay = 2
-
-        connection = pika.BlockingConnection(params)
-        channel = connection.channel()
-        channel.queue_declare(queue='python-testing')  # type: ignore
-
+        channel.exchange_declare(  # type: ignore
+            exchange=exchange_name,
+            exchange_type=exchange_type,
+            durable=True,
+        )
+        logger.info("EXCHANGE_NAME_DECLARED_OK=True")
+        return True
     except Exception as err:
-        logger.error("Could not establish connection to backend | err=%s", err, exc_info=True)
+        logger.critical("Error declaring exchange %s err=%s", exchange_type, err)
+        return False
 
 
-    if channel:
+@tl.job(interval=timedelta(seconds=10))  # type: ignore
+def create_job():
+    global global_channel, global_exchange_name, global_exchange_type
+    try:
+        jobs = Jobs()
+        message = jobs.generate_metrics()
+        global_channel.basic_publish(exchange=global_exchange_name, routing_key='', body=str(message))
+        logger.info("Message sent to consumer -> %s", message)
+        # if global_connection:
+        #     global_connection.close()
+    except Exception as err:
+        logger.error("Could not send message | err=%s", err, exc_info=True)
+
+
+def setup() -> bool:
+    """Setup the RabbitMQ connection and instantiate the global variables."""
+    global global_connection, global_channel, global_exchange_name, global_exchange_type
+    global_exchange_name = "test-exchange"
+    global_exchange_type = "topic"
+    retries = 0
+    while True:
         try:
-            jobs = Jobs()
-            message = jobs.generate_metrics()
-            channel.basic_publish(exchange='test-exchange', routing_key='', body=message)
-            logger.info("Message sent to consumer -> %s", message)
-            if connection:
-                connection.close()
-        except Exception as err:
-            logger.error("Could not send message | err=%s", err, exc_info=True)
+            url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@rabbitmq/%2f')
+            params = pika.URLParameters(url)
+            params.socket_timeout = 5
+            params.connection_attempts = 3
+            params.retry_delay = 2
 
+            global_connection = pika.BlockingConnection(params)
+            global_channel = global_connection.channel()
+            if declare_exchange(global_channel, global_exchange_name, global_exchange_type) is False:
+                raise NoExchangeException("Exchange Declaration Failed")
+            global_channel.queue_declare(queue='python-testing')  # type: ignore
+            logger.info("Publisher setup completed, now starting job timer")
+            return True
+        except Exception as err:
+            logger.warning(err, exc_info=True)
+            retries += 1
+            if retries >= 3:
+                logger.critical("RABBITMQ_CONNECTION_FAIL | MAX_RETRIES=True | err=%s", err, exc_info=True)
+                return False
+            logger.info("Waiting 30s to attempt reconnect...")
+            time.sleep(30)
+
+
+def main():
+    if setup():
+        tl.start(block=True)  # type: ignore
+    sys.exit(0)
 
 if __name__ == "__main__":
-    tl.start(block=True)
+    main()
--- rust-subscriber/src/main.rs
+++ rust-subscriber/src/main.rs
@@ -1,4 +1,9 @@
-use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Result};
+use amiquip::{
+    Connection, ConsumerMessage, ConsumerOptions, ExchangeDeclareOptions, FieldTable, QueueDeclareOptions, Result
+};
+use std::thread;
+use std::time::Duration;
+use std::str;
 
 fn split_message(input_string: &str) -> Vec<String> {
     let vec = input_string.split_whitespace().map(str::to_string).collect();
@@ -6,32 +11,77 @@ fn split_message(input_string: &str) -> Vec<String> {
 }
 
 fn main() -> Result<()> {
-    // Open connection.
-    let mut connection = Connection::insecure_open("amqp://guest:guest@rabbitmq:5672")?;
+    // Open connection with retries
+    let mut retries = 0;
+    let exchange_name = "test-exchange";
+    let queue_name = "python-testing";
+    loop {
+        match Connection::insecure_open("amqp://guest:guest@rabbitmq:5672") {
+            Err(err) => {
+                if retries >= 3 {
+                    panic!("Cannot Connect To RabbitMQ | retries exceeded!");
+                } else {
+                    println!("Cannot Connect To RabbitMQ | err={:?} retry=True | waiting 30s to try again...", err);
+                    thread::sleep(Duration::from_secs(30));
+                    retries += 1;
+                }
+            },
+            Ok(mut connection) => {
+                println!("Connection OK!");
+                let channel = connection.open_channel(None)?;
+                // Declare the queue
+                let queue = channel.queue_declare(
+                    queue_name,
+                    QueueDeclareOptions {
+                        durable: false,
+                        exclusive: false,
+                        auto_delete: false,
+                        arguments: FieldTable::default(),
+                    },
+                )?;
+                // Declare the exchange
+                let exchange = channel.exchange_declare(
+                    amiquip::ExchangeType::Topic,
+                    exchange_name,
+                    ExchangeDeclareOptions {
+                        durable: false,
+                        auto_delete: false,
+                        internal: false,
+                        arguments: FieldTable::default(),
+                    },
+                )?;
+                // Declare the exchange
+                channel.exchange_declare(
+                    amiquip::ExchangeType::Topic,
+                    exchange_name,
+                    ExchangeDeclareOptions {
+                        durable: false,
+                        auto_delete: false,
+                        internal: false,
+                        arguments: FieldTable::default(),
+                    },
+                )?;
+                queue.bind(&exchange, "", FieldTable::new())?;
+                // Start a consumer.
+                let consumer = queue.consume(ConsumerOptions::default())?;
+                println!("Waiting for messages. Press Ctrl-C to exit.");
 
-    // Open a channel - None says let the library choose the channel ID.
-    let channel = connection.open_channel(None)?;
-
-    // Declare the "hello" queue.
-    let queue = channel.queue_declare("python-testing", QueueDeclareOptions::default())?;
-
-    // Start a consumer.
-    let consumer = queue.consume(ConsumerOptions::default())?;
-    println!("Waiting for messages. Press Ctrl-C to exit.");
-
-    for (_i, message) in consumer.receiver().iter().enumerate() {
-        match message {
-            ConsumerMessage::Delivery(delivery) => {
-                let body = String::from_utf8_lossy(&delivery.body);
-                let vector_of_strings = split_message(&body.to_string());
-                println!("RUST_MESSAGE_RECEIVED -> {:?}", vector_of_strings);
-                consumer.ack(delivery)?;
-            }
-            other => {
-                println!("Consumer ended: {:?}", other);
-                break;
+                for message in consumer.receiver().iter() {
+                    match message {
+                        ConsumerMessage::Delivery(delivery) => {
+                            let body = String::from_utf8_lossy(&delivery.body);
+                            let vector_of_strings = split_message(body.as_ref());
+                            println!("RUST_MESSAGE_RECEIVED -> {:?}", vector_of_strings);
+                            consumer.ack(delivery)?;
+                        }
+                        other => {
+                            println!("Consumer ended: {:?}", other);
+                            break;
+                        }
+                    }
+                }
+                connection.close()?
             }
         }
     }
-    connection.close()
 }
